f6ae30db67258f004ec7f438b4b787ca0b20b245
ayubdev <ayubgardy898@gmail.com>
2022-09-02 21:07:37 +0300

    Done the assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const newTask = document.getElementById("new-task-description");
+    const task = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "X";
+    deleteButton.addEventListener("click", function (event) {
+      event.target.parentNode.remove();
+    });
+    task.innerText = newTask.value;
+    task.appendChild(deleteButton);
+    taskList.appendChild(task);
+    event.target.reset();
+  });
 });
